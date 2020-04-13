30bf83f16fe68316ce62205949291a83e7a1ea30
Carlos da Costa <carlosdaviddacosta@gmail.com>
2020-04-13 06:04:33 +0200

    preparing for deployment

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'../public')))
-// app.set('view engine', 'html')
+
 
 app.get('/',(req,res) => {
     res.render('index')
