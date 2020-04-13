1beadda3d9fac800c91610722c2aec8bd1717c51
Carlos da Costa <carlosdaviddacosta@gmail.com>
2020-04-13 05:15:49 +0200

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,37 @@
+document.getElementById('form').addEventListener('submit', async e => {
+    e.preventDefault()
+    let phone = e.target.elements[0].value
+    let airtime = e.target.elements[1].value
+    console.log("phone: "+phone+" airtime: "+airtime)
+    const data = {
+        phone,
+        airtime
+    }
+    let result = await request('http://localhost:3000/buy',data)
+    redirect(result)
+
+})
+
+async function request(url,data){
+    let response = await fetch(url,{
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    return response.json()
+}
+
+async function redirect(data){
+    // {PAYGATE_ID: "10011072130", PAY_REQUEST_ID: "A3A958FA-00A9-22EC-3001-74194F7B04D2", CHECKSUM: "c55c5bb3be41c2abff2357f7d6f2cb26", REFERENCE: "pgtest_123456789"}PAYGATE_ID: "10011072130"PAY_REQUEST_ID: "A3A958FA-00A9-22EC-3001-74194F7B04D2"CHECKSUM: "c55c5bb3be41c2abff2357f7d6f2cb26"REFERENCE: "pgtest_123456789"__proto__: Object
+    let form = document.getElementById('redirect')
+    form.elements[0].value = data.PAY_REQUEST_ID
+    form.elements[1].value = data.CHECKSUM
+    form.submit()
+
+}
+
---
+++ src/index.js
@@ -0,0 +1,79 @@
+const axios = require('axios')
+const c = require('crypto-js')
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname,'../public')))
+// app.set('view engine', 'html')
+
+app.get('/',(req,res) => {
+    res.render('index')
+})
+app.post('/success',(req, res) => {
+    res.sendFile(path.join(__dirname+'/../public/success.html'))
+})
+
+app.post('/buy', async (req, res) =>{
+
+    console.log(req.body)
+    const data = {
+        PAYGATE_ID: 10011072130,
+        REFERENCE: 'pgtest_123456789',
+        AMOUNT: req.body.airtime,
+        CURRENCY: 'NAD',
+        RETURN_URL: 'http://localhost:3000/success',
+        TRANSACTION_DATE: '2018-01-01 12:00:00',
+        LOCALE: 'en',
+        COUNTRY: 'NAM',
+        EMAIL: 'customer@paygate.co.za',
+    }
+    
+    function objectToString(obj){
+        let str = ''
+        for(let key in obj){
+            str += obj[key]
+        }
+        return str
+    }
+    function http_build_query(obj){
+        let str = ''
+        for(let key in obj){
+            str += key+"="+obj[key]+"&"
+        }
+        str = str.substring(0,str.length - 1)
+        return str
+    }
+    
+    let checksum = c.MD5(objectToString(data)+"secret").toString()
+    
+    data['CHECKSUM'] = checksum
+    
+    
+    let response = await axios.post('https://secure.paygate.co.za/payweb3/initiate.trans',http_build_query(data))
+
+    let arrayData = response.data.split('&')
+    console.log(response.data)
+    let resData = {
+        PAYGATE_ID: arrayData[0].split('=')[1],
+        PAY_REQUEST_ID: arrayData[1].split('=')[1],
+        CHECKSUM: arrayData[3].split('=')[1],
+        REFERENCE: arrayData[2].split('=')[1],
+        
+    }
+
+    // let redrict = await axios.post('https://secure.paygate.co.za/payweb3/process.trans',http_build_query(resData))
+    // console.log(redrict)
+    res.send(resData)
+    // res.redirect('https://secure.paygate.co.za/payweb3/process.trans')
+    // console.log('https://secure.paygate.co.za/payweb3/process.trans?'+http_build_query(resData))
+
+})
+
+app.listen(port, () =>{
+    console.log("app runing on port "+port)
+})
